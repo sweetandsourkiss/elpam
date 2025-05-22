2b9267ec203379f2de4618b9f67ee2630642d65b
sweetandsourkiss <mooul0529@naver.com>
2025-05-22 17:17:49 +0900

    Chore: Update Elpam path

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:elpam/%08elpam.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:elpam/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
