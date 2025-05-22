33595dade0129b696985e1036cc10e479a41682f
Jay Son <mooul0529@naver.com>
2025-05-23 01:15:35 +0900

    Feat: Add ocid api

---
+++ lib/providers/apis/ocid.dart
@@ -0,0 +1,12 @@
+import 'package:elpam/constants/headers.dart';
+import 'package:elpam/constants/strings.dart';
+import 'package:http/http.dart' as http;
+
+Future<String> getOcid(String name) async {
+  final uri = Uri.https(authority, '/maplestory/v1/id', {
+    'character_name': name,
+  });
+  final response = await http.get(uri, headers: commonHeader);
+
+  return '';
+}
