295035b7c992d0b3b54857cd5828a7a5aef49836
Jay Son <mooul0529@naver.com>
2025-05-22 22:32:47 +0900

    Rename: Update elpam path

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:elpam/%08elpam.dart';
+import 'package:elpam/elpam.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:elpam/%08elpam.dart';
+import 'package:elpam/elpam.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
