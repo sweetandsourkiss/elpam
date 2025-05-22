26d64f90736789f2d69eed03964c396ca008c01f
sweetandsourkiss <mooul0529@naver.com>
2025-05-22 15:48:00 +0900

    Set default settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Elpam());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Elpam extends StatelessWidget {
+  const Elpam({super.key});
 
   // This widget is the root of your application.
   @override
