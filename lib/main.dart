5d3440efd2600fbc7439a960d32bec3e14841b62
Jay Son <mooul0529@naver.com>
2025-06-07 01:06:00 +0900

    Feat: Update .env route

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  await dotenv.load(fileName: 'config/.env');
+  await dotenv.load(fileName: 'assets/config/.env');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(ProviderScope(child: const Elpam()));
 }
